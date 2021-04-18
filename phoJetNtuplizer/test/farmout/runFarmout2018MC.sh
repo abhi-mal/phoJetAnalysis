f3efef9242030ac9949bcdd512cfd4f927c0fb4e
AbhiMal-HepWisc <abhishikthmallampalli@gmail.com>
2021-04-18 11:46:17 -0500

    fix syntax issue from previous commit: bash doesn't accept comments in multiline call

--- phoJetNtuplizer/test/farmout/runFarmout2018MC.sh
+++ phoJetNtuplizer/test/farmout/runFarmout2018MC.sh
@@ -96,13 +96,13 @@ for sampleName in "${datasets[@]}"; do
     echo submitting jobs for "$sampleName"  with out dir name "${dirName[$index]}"
 
     farmoutAnalysisJobs \
-	--input-files-per-job=20 \
+	--input-files-per-job=1 \
 	--input-file-list=${inputfiles} \
 	--extra-inputs=Autumn18_V19_MC.db \
 	--extra-usercode-files=cfipython \
 	--memory-requirements=4000 \
 	--assume-input-files-exist \
-	--base-requirements='TARGET.PoolName == "HEP" && ((MY.RequiresSharedFS =!= true || TARGET.HasAFS_OSG) && (TARGET.OSG_major =!= undefined || TARGET.IS_GLIDEIN =?= true) && (TARGET.HasParrotCVMFS =?= true || (TARGET.UWCMS_CVMFS_Exists && TARGET.CMS_CVMFS_Exists && TARGET.UWCMS_CVMFS_Revision >= 444 && TARGET.CMS_CVMFS_Revision >= 81620))) && ((MY.NoAutoRequirements =?= true || (TARGET.OSglibc_major == 2 && TARGET.OSglibc_minor >= 17 && (MY.HEP_VO =!= "uscms" || TARGET.CMS_CVMFS_Exists && TARGET.UWCMS_CVMFS_Exists)))) && (TARGET.Arch == "X86_64") && (TARGET.OpSys == "LINUX") && (TARGET.HasFileTransfer)' \ #(TARGET.Disk >= RequestDisk) && (TARGET.Memory >= RequestMemory) && (TARGET.HasFileTransfer)' \ # because TARGET.Disk and TARGET.Memory are obsolete
+	--base-requirements='TARGET.PoolName == "HEP" && ((MY.RequiresSharedFS =!= true || TARGET.HasAFS_OSG) && (TARGET.OSG_major =!= undefined || TARGET.IS_GLIDEIN =?= true) && (TARGET.HasParrotCVMFS =?= true || (TARGET.UWCMS_CVMFS_Exists && TARGET.CMS_CVMFS_Exists && TARGET.UWCMS_CVMFS_Revision >= 444 && TARGET.CMS_CVMFS_Revision >= 81620))) && ((MY.NoAutoRequirements =?= true || (TARGET.OSglibc_major == 2 && TARGET.OSglibc_minor >= 17 && (MY.HEP_VO =!= "uscms" || TARGET.CMS_CVMFS_Exists && TARGET.UWCMS_CVMFS_Exists)))) && (TARGET.Arch == "X86_64") && (TARGET.OpSys == "LINUX") && (TARGET.HasFileTransfer)' \
 	${dirName[$index]} \
 	/nfs_scratch/mallampalli/Analysis/Mono-zprime/Signal_generation/GEN-SIM/full_Reco/ntuplize/CMSSW_10_2_18 \
 	$PWD/run_102X_mc2018-farmout.py
